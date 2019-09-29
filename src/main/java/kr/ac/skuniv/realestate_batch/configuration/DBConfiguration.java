e25d2295d1ecf83210fe1a777a6423c657b4b7af
kimYSang <qwdbstkd@naver.com>
2019-09-29 23:14:43 +0900

    data writer 구현

--- src/main/java/kr/ac/skuniv/realestate_batch/configuration/DBConfiguration.java
+++ src/main/java/kr/ac/skuniv/realestate_batch/configuration/DBConfiguration.java
@@ -1,15 +1,44 @@
 package kr.ac.skuniv.realestate_batch.configuration;
 
+
+import com.mysql.cj.jdbc.MysqlXADataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DBConfiguration {
 
-//    @Bean
-//    public DataSource dataSource() {
-//
-//    }
+    @Autowired
+    private Environment env;
+
+    /**
+     * Siphon data source.
+     *
+     * @return the data source
+     */
+    @Bean(name = "mainDataSource")
+    @Primary
+    public DataSource mainDataSource() {
+
+        final String user = this.env.getProperty("spring.datasource.username");
+        final String password = this.env.getProperty("spring.datasource.password");
+        final String url = this.env.getProperty("spring.datasource.jdbc-url");
+
+        return this.getMysqlXADataSource(url, user, password);
+    }
+
+    private MysqlXADataSource getMysqlXADataSource(final String url, final String user, final String password) {
+
+        final MysqlXADataSource mysql = new MysqlXADataSource();
+        mysql.setUser(user);
+        mysql.setPassword(password);
+        mysql.setUrl(url);
+
+        return mysql;
+    }
 }
