7de6542a4d59881bd29c0b006ffab873b9800c38
kimYSang <qwdbstkd@naver.com>
2019-11-10 22:55:08 +0900

    - 데이터 넣기 완료

--- src/main/java/kr/ac/skuniv/realestate_batch/RealestateBatchApplication.java
+++ src/main/java/kr/ac/skuniv/realestate_batch/RealestateBatchApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableBatchProcessing
+@EnableScheduling
 public class RealestateBatchApplication {
     public static void main(String[] args){
         SpringApplication.run(RealestateBatchApplication.class, args);
--- src/main/java/kr/ac/skuniv/realestate_batch/batch/RealestateJobConfiguration.java
+++ src/main/java/kr/ac/skuniv/realestate_batch/batch/RealestateJobConfiguration.java
@@ -8,12 +8,13 @@ import kr.ac.skuniv.realestate_batch.batch.item.RealestatePartitioner;
 import kr.ac.skuniv.realestate_batch.domain.dto.openApiDto.BuildingDealDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
@@ -22,10 +23,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -39,13 +43,45 @@ public class RealestateJobConfiguration extends DefaultBatchConfigurer {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final JobLauncher jobLauncher;
 
-    private final DataWriteTasklet dataWriteTasklet;
     private final DataSource dataSource;
+    private Integer startYear = 2019;
+    private Integer startMonth = 2;
+
+//    @Scheduled(fixedRate = 5000 )
+//    public void getData() {
+//        try {
+////            String stringParam = String.valueOf(startYear) + "0" +String.valueOf(startMonth);
+////            Long curruntParam = Long.valueOf(stringParam);
+//            List<Integer> year = new ArrayList<>();
+//
+//            JobParameters jobParameters = new JobParametersBuilder().addLong(
+//                    "requestDate", Long.valueOf(201904)).addLong(
+//                    "requestDate", Long.valueOf(201905)).toJobParameters();
+//            jobLauncher.run(apiCallJob(), jobParameters);
+//            System.out.println("I have been scheduled with Spring scheduler");
+////            --startMonth;
+////            if(startMonth == 0){
+////                return;
+////            }
+////            if(startMonth == 0) {
+////                --startYear;
+////                startMonth = 12;
+////            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
 
     @Bean
     public Job apiCallJob() {
         log.warn("----------api call job");
+//        return jobBuilderFactory.get("apiCallJob")
+//                .incrementer(new RunIdIncrementer()).listener(listener())
+//                .flow(apiCallPartitionStep()).end().build();
+
         return jobBuilderFactory.get("apiCallJob")
                 .start(apiCallPartitionStep())
                 //.next(dataWritePartitionStep())
@@ -87,7 +123,10 @@ public class RealestateJobConfiguration extends DefaultBatchConfigurer {
 //                .tasklet(dataWriteTasklet)
 //                .build();
 //    }
-
+    @Bean
+    public JobExecutionListener listener() {
+        return new JobListener();
+    }
     @Bean
     @Primary
     public JpaTransactionManager jpaTransactionManager() {
